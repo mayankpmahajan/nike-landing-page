2d929917b47dbc576847b2d139c17a67e59f341c
Mayank Mahajan <mayankpmahajan@gmail.com>
2024-06-24 19:10:40 +0530

    hero section added

---
+++ src/sections/PopularProducts.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { products } from '../constants'
+
+
+const PopularProducts = () => {
+  return (
+    <section className="max-container max-sm:mt-12" id="products">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
+
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, id. Delectus expedita rerum accusamus ad quas veniam iusto eaque. Similique deserunt reprehenderit qui iusto, consequuntur fugiat perspiciatis recusandae animi. Eos modi quisquam eum cum, corporis mollitia. Fugiat, impedit? Et corrupti praesentium optio nam natus harum accusamus cupiditate provident architecto odit?</p>
+
+        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+          {
+            
+          }
+        </div>
+        
+      </div>
+    </section>
+  )
+}
+
+export default PopularProducts
